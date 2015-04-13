11a4909209c06828904f77da364f340002ec0b72
dimmetrius <dimmetrius@gmail.com>
2015-04-13 16:39:49 +0300

    last

--- script.js
+++ script.js
@@ -8,10 +8,6 @@
       templateUrl: 'templates/sign-in.html',
       controller: 'SignInCtrl'
     })
-    .state('forgotpassword', {
-      url: '/forgot-password',
-      templateUrl: 'templates/forgot-password.html'
-    })
     .state('chatlist', {
       url: '/chatlist',
       templateUrl: 'templates/chatlist.html',
@@ -36,5 +32,45 @@
   
   $scope.signOut = function() {
       $state.go('signin');
-  };  
+  };
+
+  $scope.items =
+                [
+                   {  
+                      "title":"Запросы чата",
+                      "count":2,
+                      "details":[  
+                         {  
+                            "title":"Нижний новгород, 1234",
+                            "count":1,
+                            "badge":"badge-assertive",
+                            "text":"11:41 Явно абсурдный вопрос, который стоит блокировать"
+                         },
+                         {  
+                            "title":"Гость 1235",
+                            "count":3,
+                            "badge":"badge-assertive",
+                            "text":"11:41 Явно абсурдный вопрос, который стоит блокировать"
+                         }
+                      ]
+                   },
+                   {  
+                      "title":"Диалоги",
+                      "count":10,
+                      "details":[  
+                         {  
+                            "title":"Василий, Yandex",
+                            "count":1,
+                            "badge":"badge-balanced",
+                            "text":"11:41 Как Скоро ждать доставки? Ведь красные кактусы отцветают"
+                         },
+                         {  
+                            "title":"Светлана",
+                            "count":1,
+                            "badge":"badge-balanced",
+                            "text":"11:41 Вы продаете красные кактусы?"
+                         }
+                      ]
+                   }
+                ]
 });
